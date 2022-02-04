9966601a52597c8bd89735256a5be97b8973deb2
jc21-190507 <75290809+jc21-190507@users.noreply.github.com>
2022-02-04 11:28:01 +0900

    プロジェクト ファイルを追加します。

---
+++ Kuriage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211210
+{
+    public class Kuriage
+    {
+        public static int kuriage10(int i)
+        {
+            int surplus = i % 10;
+            
+            if(surplus != 0)
+            {
+                i = (i - surplus) + 10;
+            }
+
+            return i;
+
+        }
+    }
+}
